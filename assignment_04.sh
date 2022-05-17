9e7fdd130312799d6ba4d109c7eebd3fb9f2b78c
chiawang <chiawa@outlook.com>
2022-05-16 20:33:50 -0700

    Update assignment_04.sh

--- assignment_04.sh
+++ assignment_04.sh
@@ -1,2 +1,5 @@
 # Downloads the plain text of Guliver's Travels from Project Gutenberg
 curl https://www.gutenberg.org/files/829/829-0.txt -o ~/Documents/output.txt
+
+# Calculates the first five prime numbers and makes fives directories with these as names
+# Divides the Gulivers Travels text into approximately equal parts and puts these parts into the directories with the sequently parts as [1-5]
