eb05b6c0a3d6a510d2b6a91ef420e97afb2e2aa4
chiawang <chiawa@outlook.com>
2022-05-16 21:55:30 -0700

    Update assignment_04.sh

--- assignment_04.sh
+++ assignment_04.sh
@@ -1,7 +1,27 @@
+#!/bin/bash
 # Downloads the plain text of Guliver's Travels from Project Gutenberg
 curl https://www.gutenberg.org/files/829/829-0.txt -o output.txt
 
 # Calculates the first five prime numbers and makes fives directories with these as names
+
+n=8
+for((i=1; i<=$n/2; i++))
+do 
+  ans=$((n%i))
+  if[ $ans -eq 0]
+  then 
+    echo "$n is not a prime number."
+    exit 0
+  fi
+done
+echo "$n is a prime number."
+a= "folder${n}"
+mkdir a
+
+
+
+
+
 split --verbose --number=1/5 output.txt split
 ls -ltr | tail -5
 
