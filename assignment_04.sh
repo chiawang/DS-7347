e775735f7e2dd77a643649e090b859c26edd0f7c
chiawang <chiawa@outlook.com>
2022-05-16 21:42:30 -0700

    Update assignment_04.sh

--- assignment_04.sh
+++ assignment_04.sh
@@ -2,4 +2,12 @@
 curl https://www.gutenberg.org/files/829/829-0.txt -o ~/Documents/output.txt
 
 # Calculates the first five prime numbers and makes fives directories with these as names
+split --verbose --number=1/5 output.txt split
+ls -ltr | tail -5
+
 # Divides the Gulivers Travels text into approximately equal parts and puts these parts into the directories with the sequently parts as [1-5]
+mv splitaa folder1/
+mv splitab folder2/
+mv splitac folder3/
+mv splitad folder4/
+mv splitad folder5/
